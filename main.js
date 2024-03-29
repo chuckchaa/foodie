5b198b98e64433d19b22d0ecdca9102b72142b30
Maks <mgunko452@gmail.com>
2024-03-29 09:02:27 +0200

    added form validation (практична 4)

--- main.js
+++ main.js
@@ -6,4 +6,45 @@ document.addEventListener('DOMContentLoaded', () => {
     this.classList.toggle('burger_active')
     headerNav.classList.toggle('header__nav_active')
   })
+
+  const form = document.forms['contact-form']
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let isError = false
+
+    const name = this.elements.name.value.trim()
+    const nameError = document.querySelector('#name + .errorMessage')
+    if (name === '') {
+      nameError.textContent = 'Name is required.'
+      isError = true
+    } else {
+      nameError.textContent = ''
+    }
+
+    const email = this.elements.email.value.trim()
+    const emailError = document.querySelector('#email + .errorMessage')
+    if (email === '') {
+      emailError.textContent = 'Email is required.'
+      isError = true
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      emailError.textContent = 'Please enter a valid email address.'
+      isError = true
+    } else {
+      emailError.textContent = ''
+    }
+
+    const message = this.elements.message.value.trim()
+    const messageError = document.querySelector('#message + .errorMessage')
+    if (message === '') {
+      messageError.textContent = 'Message is required.'
+      isError = true
+    } else {
+      messageError.textContent = ''
+    }
+
+    if (!isError) {
+      alert(`Name: ${name}, email: ${email}, message: ${message}`)
+    }
+  })
 })
